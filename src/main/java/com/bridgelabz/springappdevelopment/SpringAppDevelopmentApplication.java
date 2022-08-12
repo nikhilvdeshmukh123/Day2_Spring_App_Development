2135d3f3e4546f86e74081f6417a6069c6d56b4b
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-08-12 12:03:08 +0530

    Added: SpringApp to disply Hello From BridgeLabz message

---
+++ src/main/java/com/bridgelabz/springappdevelopment/SpringAppDevelopmentApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.springappdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppDevelopmentApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello from Bridgelabz");
+        SpringApplication.run(SpringAppDevelopmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bridgelabz/springappdevelopment/SpringAppDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springappdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
